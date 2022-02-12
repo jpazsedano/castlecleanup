dc612179974e1da94f52f2e43fb39c22f33e6a61
Javier Paz <jpazsedano@gmail.com>
2022-02-12 21:40:37 +0100

    Implementado un capturador de teclado y raton (incompleto y sin testear)

--- castlecleanup/Input.go
+++ castlecleanup/Input.go
@@ -4,6 +4,7 @@ package main
 import (
     "github.com/hajimehoshi/ebiten/v2"
     "github.com/hajimehoshi/ebiten/v2/inpututil"
+    "fmt"
 )
 
 // Clases que implementen EventConsumer pueden recibir eventos de entrada.
@@ -12,6 +13,13 @@ type EventConsumer interface {
     // seguir procesándose. Si devuelve un false, se para el proceso para el
     // resto de consumidores.
     ProcessEvent(e InputEvent) bool
+
+    // ID asignado al escuchar. Puede ser -1 para indicar que aún no está registrado.
+    GetConsumerId() int
+
+    // Función que establece el valor del ID de consumidor. El valor recibido 
+    // debe ser devuelto por GetConsumerId
+    SetConsumerId(int)
 }
 
 // El evento de entrada tiene un tipo que lo identifica y un posible valor
@@ -19,8 +27,22 @@ type EventConsumer interface {
 // tener uno o más valores numéricos (sticks, gatillos o clicks), pero eso
 // ya está gestionado por cada implementación.
 type InputEvent interface {
-    GetType() string
+    GetType() int
 }
+
+type KeyEvent struct {
+    eventId int
+    PressDown bool // true si es KeyDown, false si es KeyUp
+}
+
+func (e KeyEvent*) GetType() string { e.eventId }
+
+type ClickEvent struct {
+    x int
+    y int
+}
+
+func (e ClickEvent) GetType() string { Click }
 
 // Los objetos que implementen esta interfaz deben poder capturar eventos
 // de entrada desde la fuente que sea y devolver los InputEvent correspondientes.
@@ -30,9 +52,9 @@ type InputController interface {
     // Esta función registra un consumer para recibir eventos de un determinado tipo.
     RegisterEventConsumer(consumer EventConsumer, event int) int
     // Esto elimina del registro de todos los eventos al elemento.
-    UnregisterEventCustomer(customerId int) bool
+    UnregisterEventConsumer(consumerId int) bool
     // Esto otro elimina a un consumidor de un evento concreto.
-    UnregisterFromEvent(customerId int, event int) bool
+    UnregisterFromEvent(consumerId int, event int) bool
 }
 
 // Claves de los eventos en 
@@ -43,26 +65,71 @@ const Click = 2
 // sus métodos de captura de eventos pueden saber a qué evento de juego
 // corresponde cada evento de entrada. También puede tener código para
 // utilizar controladores si están disponibles.
-type KeyboardInputController struct {
-    DebugTilesetShowKey ebiten.Key
-    listeners int[]
+type KeyboardInput struct {
+    KeyMap map[int] ebiten.Key // Mapeo de evento a tecla que lo produce.
+    Listeners map[int] []EventConsumer // Mapeo de evento a consumidor que lo quiere.
+    // ¿Es necesario que esto sea concurrent-safe? Ahora mismo no lo es.
+    LastId int
 }
 
 func MakeInputController() InputController {
     // TODO: Implementar la posibilidad de utilizar otros controladores
     controller := new(InputController)
-    cotroller.DebugTilesetShowKey = ebiten.KeyF1
+    controller.KeyMap = make(map[int] ebiten.Key)
+    controller.Listeners = make(map[int] []EventConsumer)
+    controller.LastId = 0
+
+    // Asignamos los eventos a las teclas.
+    controller.KeyMap[TilesetShowKey] = ebiten.KeyF1
 
     return controller
 }
 
-func (c KeyboardInputController*) CaptureInput() InputEvent[] {
-    // 
-    if inpututil.IsKeyJustPressed(c.DebugTilesetShowKey) {
-
+func (c KeyboardInput*) CaptureInput() InputEvent[] {
+    // Capturamos teclado
+    for evID, v := range c.KeyMap {
+        if inpututil.IsKeyJustPressed(v) {
+            for _, l := range c.Listeners[evID] {
+                l.ProcessEvent(KeyEvent{evID, true})
+            }
+        }
+        if inpututil.IsKeyJustReleased(v) {
+            for _, l := range c.Listeners[evID] {
+                l.ProcessEvent(KeyEvent{evID, false})
+            }
+        }
     }
-    // 
-    if inpututil.IsKeyJustReleased(c.DebugTilesetShowKey) {
+    // Capturamos clicks
+    // TODO
+}
 
+func (c KeyboardInput*) RegisterEventConsumer(cons EventConsumer, event int) int 
+    c.Listeners[event] = append(c.Listeners[event], cons)
+    c.LastId++
+    cons.SetConsumerId(c.LastId)
+}
+
+func (c KeyboardInput*) UnregisterEventConsumer(consumerId int) bool {
+    // Para eliminar simplemente recorremos todos los eventos que tienen listener y eliminamos.
+    for k, _ := range c.Listeners {
+        c.UnregisterFromEvent(consumerId, k)
+    }
+}
+
+// TODO: Test
+func (c KeyboardInput*) UnregisterFromEvent(consumerId int, event int) bool {
+    // Si la longitud de la lista de consumidores es 1, simplemente lo eliminamos.
+    // O también si el elemento no existe.
+    if _, ok = c.Listeners[event] ; !ok || len(c.Listeners[event]) <= 1 {
+        c.Listeners[event] = []EventConsumer{}
+    } else { // Si hay más de un item, hay que encontrar al customer que hay que eliminar.
+        for i, v := range c.Listeners[event] {
+            // Cuando encontramos al consumer a eliminar, lo hacemos.
+            if v.GetConsumerId() == consumerId {
+                c.Listeners[event][i] = c.Listeners[event][len(c.Listeners[event]) -1]
+                c.Listeners[event] = c.Listeners[event][:len(c.Listeners[event]) -1]
+                break
+            }
+        }
     }
 }
