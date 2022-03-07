cdc747c0d6a745223fe4627c588992eb78303590
Javier Paz <jpazsedano@gmail.com>
2022-03-07 20:33:08 +0100

    Finalizado (aunque sin probar) mapeador de inputs
    Este permite escuchar eventos de teclado mapeados a teclas y, importante, cambiar
    estos mapeos para asignar otras teclas a dichas acciones

--- castlecleanup/inputmanager/inputmanager.go
+++ castlecleanup/inputmanager/inputmanager.go
@@ -14,42 +14,147 @@ type Action struct {
     modifier float64
 }
 
+// Definimos el modo de controlador, que puede ser por mando o por teclado.
+type CONTROLLER_MODE int
+const (
+    MODE_KEYBOARD CONTROLLER_MODE = iota
+    MODE_CONTROLLER
+)
+
+type AXIS_ID int
+const (
+    MAIN_AXIS_H AXIS_ID = iota
+    MAIN_AXIS_V
+    SECONDARY_AXIS_H
+    SECONDRAY_AXIS_V
+    LT_AXIS
+    RT_AXIS
+)
+
+const (
+    MOVE_LEFT string = "move-left"
+    MOVE_RIGHT string = "move-right"
+    JUMP string = "jump"
+    ATTACK string = "attack"
+)
+
+const STICK_DEAD_ZONE = 0.3
+
 // La estructura InputManager se encarga de almacenar 
 type InputManager struct {
-    // Mapeo de acciones realizadas con 
-    keyMap map[int]string
-    controllerMap map[int]string
+    // Mapeo de acciones realizadas con teclado.
+    keyMap map[string]ebiten.Key
+    // Mapeo de acciones realizadas con controlador.
+    controllerMap map[string]ebiten.GamepadButton
 
-    controllerMode int
+    // Modo de controlador. Indica si la última acción se ha
+    // realizado a través de teclado o de mando.
+    controllerMode CONTROLLER_MODE
+    // Definición de ejes.
+    axisMap map[AXIS_ID]int
+
+    // Flag que indica si el stick analógico debe considerarse
+    // equivalente del pad direccional.
+    analogEqualsDpad bool
+
+    // Almacena las acciones realizadas 
+    actionsDone map[string]bool
 }
 
-// 
-func MakeInputManager() *InputManager {
-    keyMap := make(map[int]string)
-    controllerMap := make(map[int]string)
+// Simplemente crea un inputmanager con parámetros por defecto.
+func MakeDefaultInputManager() *InputManager {
+    return MakeInputManager(true)
+}
 
-    // TODO: Asignar los mapeos por defecto
+// Crea un InputManager con mapeos por defecto.
+func MakeInputManager(analogEqualsDpad bool) *InputManager {
+    keyMap := make(map[string]ebiten.Key)
+    controllerMap := make(map[string]ebiten.GamepadButton)
+    axisMap := make(map[AXIS_ID]int)
+    actionsDone := make(map[string]bool)
 
-    return &InputManager{keyMap, controllerMap}
+    // TODO: Quizás los controles por defecto no deberían definirse aquí
+    // Controles por defecto de teclado.
+    keyMap[MOVE_LEFT] = ebiten.KeyArrowLeft
+    keyMap[MOVE_RIGHT] = ebiten.KeyArrowRight
+    keyMap[JUMP] = ebiten.KeyZ
+    keyMap[ATTACK] = ebiten.KeyX
+    // Controles por defecto de mando.
+    controllerMap[MOVE_LEFT] = ebiten.GamepadButton11
+    controllerMap[MOVE_RIGHT] = ebiten.GamepadButton12
+    controllerMap[JUMP] = ebiten.GamepadButton0
+    controllerMap[ATTACK] = ebiten.GamepadButton2
+    axisMap[MAIN_AXIS_H] = 0
+    axisMap[MAIN_AXIS_V] = 1
+    axisMap[SECONDARY_AXIS_H] = 3
+    axisMap[SECONDARY_AXIS_V] = 4
+    axisMap[LT_AXIS] = 2
+    axisMap[RT_AXIS] = 5
+
+    actionsDone[MOVE_LEFT] = false
+    actionsDone[MOVE_RIGHT] = false
+    actionsDone[JUMP] = false
+    actionsDone[ATTACK] = false
+
+    return &InputManager{
+        keyMap,
+        controllerMap,
+        MODE_KEYBOARD,
+        axisMap,
+        analogEqualsDpad,
+        actionsDone,
+    }
 } 
 
-// Esta función devuelve si una acción está ahora mismo realizándose. Si el botón
-// o stick 
+// Esta función devuelve si una acción está ahora mismo realizándose. Devuelve
+// nil si la acción on está realizada. Tiene en cuenta equivalencia de stick.
 func (im *InputManager) IsActionPressed(action string) *Action {
-    return nil
+    // Miramos la tecla respectiva y, si está pulsada, devolvemos el valor.
+    key, ok := im.keyMap[action]
+    // Si la acción no existe, por supuesto que no se está haciendo.
+    if !ok {
+        return nil
+    }
+
+    // Si el botón está pulsándose, se realiza.
+    if ebiten.IsKeyPressed(key) {
+        return &Action{action, 1.0} // Por el momento no hay modificador de la acción.
+    } else {
+        return nil
+    }
 }
 
+// Esta función devuelve una acción si está ahora mismo realizándose.
+// Devuelve nil si no. Tiene en cuenta equivalencia de stick.
 func (im *InputManager) IsActionJustPressed(action string) *Action {
-    return nil
+    key, ok := im.keyMap[action]
+
+    if !ok {
+        return nil
+    }
+
+    if inpututil.IsKeyJustPressed(key) {
+        return &Action{action, 1.0}
+    }
 }
 
 // Cambia los mapeos de acciones al teclado.
-func (im *InputManager) MapKey(action string, key int) bool {
+func (im *InputManager) MapKey(action string, key ebiten.Key) bool {
+    switch action {
+    // Sólo hacemos caso si es una de las acciones posibles.
+    case MOVE_LEFT, MOVE_RIGHT, JUMP, ATTACK:
+        im.keyMap[action] = key
+        return true
+    }
     return false
 }
 
 // Cambia los mapeos de acciones al controlador
-func (im *InputManager) MapButton(action string, button int) bool {
+func (im *InputManager) MapButton(action string, button ebiten.GamepadButton) bool {
+    return false
+}
+
+func (im *InputManager) MapAxis(axis AXIS_ID, padAxis int) bool {
     return false
 }
 
@@ -57,5 +162,6 @@ func (im *InputManager) MapButton(action string, button int) bool {
 // de puntero que sea.
 func (im *InputManager) PointerPosition() (int, int) {
     // TODO: ¿La función de ebiten tiene ya la funcionalidad que debe tener esta?
+    // Es decir, el abstraer cursor y pulsación en pantalla. Y ¿Queremos esa funcionalidad?
     return ebiten.CursorPosition()
 }
